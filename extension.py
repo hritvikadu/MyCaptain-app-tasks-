18fda256893f55fb1ad3e4d4b3c69455b2e1075e
hritvikadu <69305958+hritvikadu@users.noreply.github.com>
2020-08-12 16:40:15 +0530

    created extension.py
    this program accepts filename from the user and prints the extension of it.

---
+++ extension.py
@@ -0,0 +1,13 @@
+f=input("Input the file name:")
+s= f.split('.')[1]
+if s=="py":
+    print("The extension of the file is: 'python'")
+
+elif s=="js":
+     print("The extension of the file is: 'JavaScript'")
+
+elif s=="htm" or s=="html":
+     print("The extension of the file is: 'HTML'")
+
+elif s=="jpeg" or s=="jpg":
+     print("The extension of the file is: 'JPEG'")
